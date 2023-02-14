9c76dd99b1c56d830999b60131b731ed57c90c3d
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2023-02-14 14:32:33 -0500

    send help please ahhhh

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
   {
@@ -20,31 +20,67 @@ const userSchema = new Schema(
       required: true,
       minlength: 5,
     },
-    numberOfPets: {
-      type: Number,
-      required: false,
+    pet: {
+      type: Schema.Types.ObjectId,
+      ref: 'pet',
     },
-    location: {
-      type: String,
-      required: false,
+    ownerName: {
+      type: Schema.Types.ObjectId,
+      ref: "username",
     },
-    listPets: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Pet'
-      }
-    ]
+    posts: [{
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
   }
 );
 
+// Provided with Anthony's boilerplate - need to test
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
-
   next();
 });
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// POTENTIAL FRIEND COUNT FUNCTION (CHANGE STUDENT INSTANCES TO FRIEND)
+
+// const headCount = async () =>
+//   Student.aggregate()
+//     .count('studentCount')
+//     .then((numberOfStudents) => numberOfStudents);
+
+// // Aggregate function for getting the overall grade using $avg
+// const grade = async (studentId) =>
+//   Student.aggregate([
+//     // only include the given student by using $match
+//     { $match: { _id: ObjectId(studentId) } },
+//     {
+//       $unwind: '$assignments',
+//     },
+//     {
+//       $group: {
+//         _id: ObjectId(studentId),
+//         overallGrade: { $avg: '$assignments.score' },
+//       },
+//     },
+//   ]);
+
 
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
