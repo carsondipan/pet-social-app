510d79a5a849c2177c857f7b06bb185cf82dda1b
uiido <uiido@yahoo.com>
2023-02-14 19:37:07 -0500

    Added registration page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -62,22 +62,22 @@ const Login = () => {
               </h1>
               <form onSubmit={handleFormSubmit} class="space-y-4 md:space-y-6" action="#">
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
                   <input type="email" name="email" id="email"
                     value={formState.email}
                     onChange={handleChange}
-                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="true" />
                 </div>
                 <div>
                   <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                   <input type="password" name="password" id="password"
                     value={formState.password}
                     onChange={handleChange}
-                    placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                    placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="true" />
                 </div>
                 <button type="submit" class="w-full text-white bg-gray-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                 <p class="text-sm font-light text-gray-300 dark:text-gray-400">
-                  Don’t have an account yet? <a href="/signup" class="font-medium text-primary-100 hover:underline dark:text-primary-500">Sign up</a>
+                  Don’t have an account yet? <a href="/signup" class="font-medium text-primary-100 hover:underline dark:text-primary-500">Sign up!</a>
                 </p>
               </form>
             </div>
